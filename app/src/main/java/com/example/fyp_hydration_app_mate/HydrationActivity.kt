2e6d1144a8508f7ba69ee84fa57e70e85bd5b864
Maxamed Maxamed <20088349@mail.wit.ie>
2024-01-24 16:25:21 +0000

    Rename MainActivity to HydrationActivity and Refactor HydrationActivity code

---
+++ app/src/main/java/com/example/fyp_hydration_app_mate/HydrationActivity.kt
@@ -0,0 +1,38 @@
+package com.example.fyp_hydration_app_mate
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fyp_hydration_app_mate.databinding.ActivityHydrationBinding
+import timber.log.Timber
+
+class HydrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHydrationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHydrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Timber.plant(Timber.DebugTree())
+
+        binding.hydrationGoalButton.setOnClickListener {
+            val enterHydrationGoal = binding.hydrationGoalTextView.text.toString()
+
+            if (enterHydrationGoal.isEmpty()) {
+                binding.hydrationGoalTextView.error = "Enter Hydration Goal"
+                Timber.i("Hydration Goal is Empty")
+                return@setOnClickListener
+            } else {
+                binding.hydrationGoalTextView.error = null
+            }
+
+            val hydrationGoal = enterHydrationGoal.toInt()
+            Toast.makeText(this, "Hydration Goal is $hydrationGoal", Toast.LENGTH_SHORT).show()
+
+            Timber.i("Hydration Goal Button Clicked")
+            Timber.i("Hydration Goal is $hydrationGoal")
+        }
+    }
+}
