13ac095c1ea3197e4d29dd2eb9e3151989f5fcdf
Maxamed Maxamed <20088349@mail.wit.ie>
2024-01-24 13:17:46 +0000

    Update Android project files and add hydration UI.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,20 +41,6 @@ android {
 }
 
 dependencies {
-
-//    implementation("androidx.core:core-ktx:1.12.0")
-//    implementation("androidx.appcompat:appcompat:1.6.1")
-//    implementation("com.google.android.material:material:1.11.0")
-//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-
-    //firebase
-
-    //
     //other things
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -73,6 +59,4 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.7.0")
 
-
-    implementation("com.google.android.material:material:1.11.0")
 }
