c59d74b324bdc48137e0b522eda872e7b45e1eca
Maxamed Maxamed <20088349@mail.wit.ie>
2024-02-07 12:20:00 +0000

    Update AndroidManifest.xml, HydrationListActivity.kt, HydrationAdapter.kt, and activity_hydration_list.xml

--- app/src/main/java/com/example/fyp_hydration_app_mate/activitiesList/HydrationListActivity.kt
+++ app/src/main/java/com/example/fyp_hydration_app_mate/activitiesList/HydrationListActivity.kt
@@ -1,20 +1,17 @@
-package com.example.fyp_hydration_app_mate
+package com.example.fyp_hydration_app_mate.activitiesList
 import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fyp_hydration_app_mate.databinding.ActivityHydrationListBinding
 import com.example.fyp_hydration_app_mate.main.MainApp
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.fyp_hydration_app_mate.R
 import com.example.fyp_hydration_app_mate.activities.HydrationActivity
-import com.example.fyp_hydration_app_mate.databinding.CardHydrationBinding
-import com.example.fyp_hydration_app_mate.models.HydrationModel
+import com.example.fyp_hydration_app_mate.adapters.HydrationAdapter
 class HydrationListActivity : AppCompatActivity() {
 
     private lateinit var app: MainApp
@@ -69,41 +66,5 @@ class HydrationListActivity : AppCompatActivity() {
 
 
 
-
-class HydrationAdapter(private val hydrationModelMain: List<HydrationModel>) :
-    RecyclerView.Adapter<HydrationAdapter.MainHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardHydrationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MainHolder(binding)
-    }
-
-    override fun getItemCount(): Int {
-        // Return the size of your data list
-        return hydrationModelMain.size
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-
-        val hydration = hydrationModelMain[holder.adapterPosition]
-        holder.binding(hydration)
-    }
-
-
-    class MainHolder(private val binding: CardHydrationBinding) : RecyclerView.ViewHolder(binding.root)
-    {
-
-        fun binding(hydrationModel: HydrationModel)
-        {
-            binding.textViewDescription.text = hydrationModel.hydrationGoal.toString().toInt().toString()
-            binding.textViewTitle.text = hydrationModel.currentHydration.toString().toInt().toString()
-            binding.progressBar.progress = hydrationModel.currentHydration.toString().toInt()
-            binding.progressBar.max = hydrationModel.hydrationGoal.toString().toInt()
-        }
-
-
-
-
-    }
-}
 
 
---
+++ app/src/main/java/com/example/fyp_hydration_app_mate/adapters/HydrationAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.fyp_hydration_app_mate.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fyp_hydration_app_mate.databinding.CardHydrationBinding
+import com.example.fyp_hydration_app_mate.models.HydrationModel
+
+
+class HydrationAdapter(private val hydrationModelMain: List<HydrationModel>) :
+    RecyclerView.Adapter<HydrationAdapter.MainHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardHydrationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MainHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        // Return the size of your data list
+        return hydrationModelMain.size
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+
+        val hydration = hydrationModelMain[holder.adapterPosition]
+        holder.binding(hydration)
+    }
+
+
+    class MainHolder(private val binding: CardHydrationBinding) : RecyclerView.ViewHolder(binding.root)
+    {
+
+        fun binding(hydrationModel: HydrationModel)
+        {
+            binding.textViewDescription.text = hydrationModel.hydrationGoal.toString().toInt().toString()
+            binding.textViewTitle.text = hydrationModel.currentHydration.toString().toInt().toString()
+            binding.progressBar.progress = hydrationModel.currentHydration.toString().toInt()
+            binding.progressBar.max = hydrationModel.hydrationGoal.toString().toInt()
+        }
+
+
+
+
+    }
+}
