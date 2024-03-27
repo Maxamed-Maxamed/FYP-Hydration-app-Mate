54b3fea29c62fe39f52866f97a6606d9a3d916dd
Maxamed Maxamed <60884619+Maxamed-Maxamed@users.noreply.github.com>
2024-03-27 11:43:24 +0000

    Added ID generation functionality to HydrationMemStore.kt

--- app/src/main/java/com/example/fyp_hydration_app_mate/models/HydrationMemStore.kt
+++ app/src/main/java/com/example/fyp_hydration_app_mate/models/HydrationMemStore.kt
@@ -5,38 +5,30 @@ import timber.log.Timber
 import timber.log.Timber.Forest.i
 
 class HydrationMemStore : HydrationStore {
-
+    
     private val hydrationList = ArrayList<HydrationModel>()
-
+    var lastId = 0L
+    
+    internal fun getId(): Long {
+        return lastId++
+    }
 
     override fun findAll(): List<HydrationModel> {
+        // Function logic here
         return hydrationList
     }
 
     override fun create(hydrationModel: HydrationModel) {
+        hydrationModel.id = getId() // Example of utilizing the getId() function
         hydrationList.add(hydrationModel)
         i("Hydration record added: $hydrationModel")
-
     }
 
-
-//    override fun update(hydrationModel: HydrationModel) {
-//        val foundHydrationModel: HydrationModel? = hydrationList.find { p -> p.id == hydrationModel.id }
-//        if (foundHydrationModel != null) {
-//            foundHydrationModel.hydrationGoal = hydrationModel.hydrationGoal
-//        }
-//        Timber.i("Hydration record updated: $hydrationModel")
-//    }
-
-
-
-    // Implement delete and update
     override fun delete(hydrationModel: HydrationModel) {
         hydrationList.remove(hydrationModel)
     }
 
     override fun update(hydrationModel: HydrationModel) {
-
         val foundHydration = hydrationList.find { it.id == hydrationModel.id }
         if (foundHydration!= null) {
             foundHydration.hydrationGoal = hydrationModel.hydrationGoal
@@ -44,34 +36,14 @@ class HydrationMemStore : HydrationStore {
 
             foundHydration.copy(hydrationGoal = hydrationModel.hydrationGoal, id = hydrationModel.id)
 
-
             return
-
         } else {
             i("Hydration record not found")
         }
     }
 
-
-    // Log hydrations after change
     override fun logAll() {
         i("Hydration list: $hydrationList")
     }
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
